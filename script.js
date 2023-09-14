217076b2fd10cecf9acaa2862ba9d8e7f0f34e0e
kuruvasaikiran11 <kuruvasaikiran11@gmail.com>
2023-09-14 17:22:11 +0530

    Added localStorage functionality to store tasks

--- script.js
+++ script.js
@@ -4,24 +4,28 @@ let addButton = document.getElementById("add-btn");
 let listItems = document.getElementById("list-items");
 
 let taskList = JSON.parse(localStorage.getItem("taskList"));
-// console.log(taskList);
-if(taskList === null){
-    taskList = [];
-}else{
-    for(let i = 0; i < taskList.length; i++){
-        addTask(taskList[i]);
+console.log(taskList);
+document.addEventListener("DOMContentLoaded", function(){
+    if(taskList === null){
+        taskList = [];
+    }else{
+        for(let i = 0; i < taskList.length; i++){
+            addTask(taskList[i]);
+        }
     }
-}
+})
 
 addButton.addEventListener("click", function(){
     // console.log(input.value);
-    addTask(input.value);
+    const task = input.value;
+    addTask(task);
+    taskList.push(task);
+    localStorage.setItem("taskList", JSON.stringify(taskList));
 })
 
 function addTask(task){
     listItems.innerHTML += `<li>${task}</li>`;    
     input.value = "";
     input.focus();
-    taskList.push(task);
-    localStorage.setItem(taskList, taskList);
 }
+
